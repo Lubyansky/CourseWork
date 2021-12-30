95e5bb7d860a816ba62af79cc4c988675d5d3180
Lubyansky <noobsa87@gmail.com>
2021-12-30 14:08:29 +0500

    v 1.0

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -12,26 +12,4 @@ class EmailContoller {
   }
 }
 
-
-//↓↓↓↓↓↓ снимаем комментарий
-/*
-const db = require('../db.js')
-
-class EmailContoller {
-  async addEmail(req, res){
-
-    try {
-      const {email} = req.body
-      const newEmail = await db.query(`INSERT INTO public."Emails" (email_id, email) VALUES (default, $1) RETURNING *`, [email])
-      res.status(201).json(newEmail.rows[0])
-    }
-    catch(e) {
-      res.status(400).json()
-    }
-
-  }
-}
-*/
-
-//↓↓↓ не трогаем
 module.exports =  new EmailContoller()
